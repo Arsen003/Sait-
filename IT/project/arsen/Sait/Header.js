540c0add6b5e1300c7c83d24411631726a2efc5e
Arsen003 <arsmeliqyan97@gmail.com>
2020-03-23 13:50:16 +0400

    new commit

---
+++ IT/project/arsen/Sait/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Technologies from "./Technologies";
+
+const Header =() => {
+    return (
+        <div>
+            <a href="#S">Home</a>
+            <a href="#S"> Feed</a>
+            <a href="#S">Messages</a>
+        </div>);
+}
+export  default  Header;
+
---
+++ IT/project/arsen/Sait/Technologies.js
@@ -0,0 +1,14 @@
+import React from "React";
+
+const  Technologies = () =>{
+    return(
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>);
+}
+export  default Technologies;
